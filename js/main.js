a07f2cc9ef5b6186f0bd0f725c2c152c30d60a6b
Bryan McBride <mcbride.bryan@gmail.com>
2014-07-11 11:12:12 -0400

    Add MapQuest basemaps and layer control

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var map, db, lyr, mapquestOSM, mapquestOAM;
+
 function onBodyLoad() {
 	document.addEventListener("deviceready", go, false);
 }
@@ -43,21 +45,45 @@ function loadDatabase() {
 }
 
 function buildMap() {
-	var db = window.sqlitePlugin.openDatabase({
+	db = window.sqlitePlugin.openDatabase({
 		name : "Database"
 	});
-	
-	var map = new L.Map('map', {
+
+	map = new L.Map('map', {
 		center : new L.LatLng(41.311000, -72.927000),
 		zoom : 14
 	});
 
-	var lyr = new L.TileLayer.MBTiles('', {
+	lyr = new L.TileLayer.MBTiles('', {
 		maxZoom : 17,
-		scheme : 'tms'
+		scheme : 'tms'/*,
+		tms: true*/
 	}, db);
-	
-	map.addLayer(lyr);
+
+	mapquestOSM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png", {
+	  maxZoom: 19,
+	  subdomains: ["otile1", "otile2", "otile3", "otile4"],
+	  attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">. Map data (c) <a href="http://www.openstreetmap.org/" target="_blank">OpenStreetMap</a> contributors, CC-BY-SA.'
+	});
+
+	map.addLayer(mapquestOSM);
+
+	mapquestOAM = L.tileLayer("http://{s}.mqcdn.com/tiles/1.0.0/sat/{z}/{x}/{y}.jpg", {
+	  maxZoom: 18,
+	  subdomains: ["oatile1", "oatile2", "oatile3", "oatile4"],
+	  attribution: 'Tiles courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a>. Portions Courtesy NASA/JPL-Caltech and U.S. Depart. of Agriculture, Farm Service Agency'
+	});
+
+	var baseLayers = {
+	  "Street Map": mapquestOSM,
+	  "Aerial Imagery": mapquestOAM
+	};
+
+	var overlayLayers = {
+	  "Offline": lyr
+	};
+
+	L.control.layers(baseLayers, overlayLayers).addTo(map);
 }
 
 function alertHtml(content) {
